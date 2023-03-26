c4e9cf39796016db4c846636c0b843b787939ba8
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-03-26 18:45:09 +0530

    fix: remove empty flags object

--- src/commands/save/index.ts
+++ src/commands/save/index.ts
@@ -14,13 +14,6 @@ export default class Save extends Command {
 
   static examples = ["<%= config.bin %> <%= command.id %>"];
 
-  static flags = {
-    // flag with a value (-n, --name=VALUE)
-    // name: Flags.string({ char: "n", description: "name to print" }),
-    // flag with no value (-f, --force)
-    // force: Flags.boolean({ char: "f" }),
-  };
-
   static args = {
     file: Args.string({ description: "file to read" }),
     name: Args.string({ description: "name of the project" }),
