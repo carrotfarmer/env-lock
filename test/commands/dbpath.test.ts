735315f8a3393daee5ed8b3b1bad5edfcdd6f25e
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-03-30 21:30:23 +0530

    feat(test): dbpath test

---
+++ test/commands/dbpath.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from "@oclif/test";
+
+describe("dbpath", () => {
+  // TODO: Improve this test
+  test
+    .stdout()
+    .command(["dbpath"])
+    .it("runs dbpath", (ctx) => {
+      expect(ctx.stdout).to.not.be.empty;
+    });
+});
+
